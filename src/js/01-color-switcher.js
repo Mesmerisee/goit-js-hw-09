73c0e385186003ae238eb8463946a11f2b516d83
Mesmerisee <124400238+Mesmerisee@users.noreply.github.com>
2023-06-27 20:06:41 +0300

    task 1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
 
+let setIntervalEl = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+stopBtnEl.setAttribute('disabled', '');
+
+startBtnEl.addEventListener('click', el => {
+  el.target.setAttribute('disabled', 'true');
+  stopBtnEl.removeAttribute('disabled');
+  setIntervalEl = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtnEl.addEventListener('click', el => {
+  el.target.setAttribute('disabled', true);
+  startBtnEl.removeAttribute('disabled');
+  clearInterval(setIntervalEl);
+});
